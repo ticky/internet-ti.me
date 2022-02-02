729525e23772e3c2cf1d6da72865447098e68649
Jessica Stokes <hello@jessicastokes.net>
2022-02-01 20:49:06 -0800

    Add JS-powered display of the current local time in the current locale
    fixes #5

--- internet_ti_dot_me/static/js/index.js
+++ internet_ti_dot_me/static/js/index.js
@@ -1,4 +1,4 @@
-const { DateTime, FixedOffsetZone } = luxon;
+const { DateTime, Duration, FixedOffsetZone } = luxon;
 
 class BielMeanTimeZone extends FixedOffsetZone {
   static singleton = new BielMeanTimeZone()
@@ -15,7 +15,7 @@ Object.defineProperties(
       get() {
         const bielTime = this.setZone(BielMeanTimeZone.singleton);
         // Returns the current beats, to two digits of centibeats
-        return Math.round((bielTime.second + bielTime.minute * 60 + bielTime.hour * 3600) / .864) / 100;
+        return Math.round(bielTime.diff(bielTime.startOf('day')).toMillis() / 864) / 100;
       }
     },
 
@@ -38,28 +38,23 @@ Object.defineProperties(
   }
 );
 
-const referenceWrapper = document.getElementById('reference');
-const beatsHeading = document.getElementById('beats');
-const beatsPermalink = document.getElementById('permalink');
-const timeZoneElements = Array.prototype.map.call(
-  document.querySelectorAll('#time-zones > li > time'),
-  (timeWrapperElement) => ({
-    timeWrapperElement,
-    timeElement: timeWrapperElement.querySelector('span.time'),
-    zoneElement: timeWrapperElement.querySelector('span.zone')
-  })
-);
+document.addEventListener('DOMContentLoaded', () => {
+  const referenceWrapper = document.getElementById('reference');
+  const beatsHeading = document.getElementById('beats');
+  const beatsPermalink = document.getElementById('permalink');
+  const timeZoneElements = Array.prototype.map.call(
+    document.querySelectorAll('#time-zones > li > time'),
+    (timeWrapperElement) => ({
+      timeWrapperElement,
+      timeElement: timeWrapperElement.querySelector('span.time'),
+      zoneElement: timeWrapperElement.querySelector('span.zone')
+    })
+  );
 
-const beatsNumberFormatter = new Intl.NumberFormat(undefined, { minimumIntegerDigits: 3 });
-let lastBeats;
+  const beatsNumberFormatter = new Intl.NumberFormat(undefined, { minimumIntegerDigits: 3 });
+  let lastBeats;
 
-const updateTime = () => {
-  const dateTime = DateTime.now().setZone(BielMeanTimeZone.singleton);
-  const currentBeats = Math.floor(dateTime.beats);
-
-  if (currentBeats !== lastBeats) {
-    lastBeats = currentBeats;
-
+  const updateTime = (dateTime, currentBeats=Math.floor(dateTime.beats)) => {
     const beatsString = `@${beatsNumberFormatter.format(currentBeats)}`;
 
     document.title = `${beatsString} - internet-ti.me`;
@@ -71,17 +66,48 @@ const updateTime = () => {
     timeZoneElements.forEach(({ timeWrapperElement, timeElement, zoneElement }) => {
       const timeInZone = dateTime.setZone(timeWrapperElement.dataset.zone ? timeWrapperElement.dataset.zone : BielMeanTimeZone.singleton);
 
-      timeElement.innerText = timeInZone.toLocaleString({ hour: 'numeric', minute: 'numeric', hour12: false });
+      timeElement.innerText = timeInZone.toLocaleString({ hour: 'numeric', minute: 'numeric', hour12: timeWrapperElement.dataset.zone === 'local' ? undefined : false });
       timeWrapperElement.setAttribute('datetime', timeInZone.toISO());
 
       // Update the time zone names if it's got an associated IANA zone name
       if (timeWrapperElement.dataset.zone) {
-        zoneElement.innerText = timeInZone.localTimeZoneName;
+        if (timeWrapperElement.dataset.zone !== 'local') {
+          zoneElement.innerText = timeInZone.localTimeZoneName;
+        } else {
+          zoneElement.innerText = timeInZone.timeZoneName;
+        }
+      }
+
+      if (timeWrapperElement.parentElement.style.display == 'none') {
+        timeWrapperElement.parentElement.style.display = '';
       }
     });
+  };
+
+  const liveUpdateTime = () => {
+    const dateTime = DateTime.now().setZone(BielMeanTimeZone.singleton);
+    const currentBeats = Math.floor(dateTime.beats);
+
+    if (currentBeats !== lastBeats) {
+      lastBeats = currentBeats;
+      updateTime(dateTime, currentBeats);
+    }
+
+    window.requestAnimationFrame(liveUpdateTime);
+  };
+
+  if (referenceWrapper.dataset.live === 'true') {
+    liveUpdateTime();
+  } else {
+    const currentBeats = parseInt(referenceWrapper.dataset.beats, 10);
+
+    const dateTime = (
+      DateTime.fromISO(document.querySelector('time[datetime$="+01:00"]').dateTime)
+        .setZone(BielMeanTimeZone.singleton)
+        .startOf('day')
+        .plus(currentBeats * 86400)
+    );
+
+    updateTime(dateTime);
   }
-
-  window.requestAnimationFrame(updateTime);
-};
-
-updateTime();
+});
